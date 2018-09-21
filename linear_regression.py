516cbaf4572d5c5f4d0b07c5e37a9f0d00ae7bc8
angelbaowei <33621625+angelbaowei@users.noreply.github.com>
2018-09-21 20:05:45 +0800

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,89 +1,105 @@
-# 单特征 线性回归
-
 import numpy as np
 from matplotlib import pyplot as plt
+
+N = 14  # sample number
+M = 1  # feature number
 
 l = []
 for i in range(2000, 2014):
     l.append(i)
 
 x = np.array(l, dtype=np.float32)
+x = np.expand_dims(x, axis=-1)
+x0 = np.ones(shape=[N, 1], dtype=np.float32)
+X = np.concatenate([x0, x], axis=-1)  # shpe = N * (M+1)
+print('X shape: ', X.shape)
 y = np.array([2.000, 2.500, 2.900, 3.147, 4.515, 4.903, 5.365, 5.704, 6.853, 7.971, 8.561, 10.000, 11.280, 12.900], dtype=np.float32)
+y = np.expand_dims(y, axis=-1)
+print('y shape:', y.shape)
 mean_x, std_x = np.mean(x), np.std(x)
+print('x mean, std ', mean_x, std_x)
 mean_y, std_y = np.mean(y), np.std(y)
-print(0.838/std_x)
+print('y mean, std ', mean_y, std_y)
 
-old_x = x
-old_y = y
-x = (x - mean_x) / std_x
-y = (y - mean_y) / std_y
-m = 14
-gt_w = 1.1
-gt_b = 0
+# h(w) = X * sita
+sita = np.zeros(shape=[M + 1, 1], dtype=np.float32)
 
-plt.scatter(x, y)
-plt.plot(x, 1.1 * x)  # 假设 的 ground_truth
-plt.xlabel('Year')
-plt.ylabel('Price')
-# plt.show()
-
-# h(w) = w * x + b
-# J(w, b) = 1/2 * (h(w) - y_)^T * (h(w) - y_)
-w = np.random.uniform(0, 2)
-w = np.array(w, dtype=np.float32)
-b = np.random.uniform(-3, 3)
-b = np.array(b, dtype=np.float32)
-print(w, b)
+count_list = []
+loss_list = []
+sita0_list = []
+sita1_list = []
 
 
-def compute_loss(x, w, b, y_):
-    loss = 0
-    h = np.dot(np.transpose(w), x) + b
-    # print(h)
-    for i in range(m):
-        loss += 1/2 * np.dot(np.transpose(h-y_), h-y_)
+def GD(sita):
+    epoch = 3500000
+    lr = 4e-7
+    for i in range(epoch + 1):
+        h = np.dot(X, sita)
+        loss = 0
+        grad = np.zeros(shape=[M + 1, 1], dtype=np.float32)
 
-    loss /= m
-    return loss
+        loss = 0.5 * np.dot((h - y).T, h - y)[0][0]
+        grad = (np.dot(X.T, h - y))
 
+        loss /= N
+        grad /= N
 
-lr_w = lr_b = 8e-1
-for i in range(500):
-    loss = compute_loss(x, w, b, y)
-    print('step = %d, loss = %f' % (i+1, loss))
+        if i % 2000 == 0:
+            count_list.append(i)
+            loss_list.append(loss)
+            sita0_list.append(sita[0][0])
+            sita1_list.append(sita[1][0])
+            print('step = %d, loss = %f, sita0 = %f, sita1 = %f' % (i, loss, sita[0][0], sita[1][0]))
 
-    h = np.dot(np.transpose(w), x) + b
+        sita[0][0] = sita[0][0] - lr * grad[0][0] * 9e5
+        sita[1][0] = sita[1][0] - lr * grad[1][0]
 
-    grid_w = 0
-    grid_b = 0
-    for i in range(m):
-        grid_w += ((h[i]-y[i]) * x[i])
-        grid_w /= m
-        grid_b += (h[i]-y[i])
-        grid_b /= m
-        grid_w = np.fabs(grid_w)
-        grid_b = np.fabs(grid_b)
+    #sita[0][0] = -1599
+    #sita[1][0] = 0.8
+    return sita
 
-    print('grid_w = %f grid_b = %f' % (grid_w, grid_b))
-
-    if w < gt_w:
-        grid_w *= -1
-    if b < gt_b:
-        grid_b *= -1
-
-    w = w - lr_w * grid_w
-    b = b - lr_b * grid_b
-    print('w = %f b = %f' % (w, b))
+def formula(sita):
+    #  正规方程
+    XTX = np.dot(X.T, X)
+    ni = np.linalg.inv(XTX)
+    sita = np.dot(np.dot(ni, X.T), y)
+    return sita
 
 
+def result(sita):
+    plt.figure()
+    plt.scatter(x, y)
+    plt.xlabel('Year')
+    plt.ylabel('Price')
+    print(sita)
+    plt.plot(x, sita[1][0] * x + sita[0][0])
 
-input = [2014, 2015, 2016, 2017, 2018, 2019, 2020]
-input = (input - mean_x) / std_x
-output = (w * input + b)
-output = (output * std_y + mean_y)
-print(output)
+    plt.figure()
+    fg2, axes = plt.subplots(2, 2)
+    fg_loss = axes[0][0]
+    fg_sita0 = axes[1][0]
+    fg_sita1 = axes[1][1]
+    fg_loss.set_xlabel('step')
+    fg_loss.set_ylabel('loss')
+    fg_sita0.set_xlabel('step')
+    fg_sita0.set_ylabel('sita0')
+    fg_sita1.set_xlabel('step')
+    fg_sita1.set_ylabel('sita1')
+    fg_loss.plot(count_list, loss_list)
+    fg_sita0.plot(count_list, sita0_list)
+    fg_sita1.plot(count_list, sita1_list)
+
+    plt.show()
 
 
-plt.plot(x, w * x + b)
-plt.show()
+def main():
+    gd = GD(sita); result(gd)
+    #form = formula(sita); result(form)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
 
