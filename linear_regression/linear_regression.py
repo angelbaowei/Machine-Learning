0f6a58449e54dac742a4716a783aa5a7a83d9461
angelbaowei <33621625+angelbaowei@users.noreply.github.com>
2018-10-04 18:36:02 +0800

    Update linear_regression.py

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -11,30 +11,37 @@ for i in range(2000, 2014):
 x = np.array(l, dtype=np.float32)
 x = np.expand_dims(x, axis=-1)
 x0 = np.ones(shape=[N, 1], dtype=np.float32)
+
+mean_x, std_x = np.mean(x), np.std(x)
+print('x mean, std ', mean_x, std_x)
+
+print('norm')
+x = (x - mean_x) / std_x
+
 X = np.concatenate([x0, x], axis=-1)  # shpe = N * (M+1)
 print('X shape: ', X.shape)
 y = np.array([2.000, 2.500, 2.900, 3.147, 4.515, 4.903, 5.365, 5.704, 6.853, 7.971, 8.561, 10.000, 11.280, 12.900], dtype=np.float32)
 y = np.expand_dims(y, axis=-1)
 print('y shape:', y.shape)
-mean_x, std_x = np.mean(x), np.std(x)
-print('x mean, std ', mean_x, std_x)
 mean_y, std_y = np.mean(y), np.std(y)
 print('y mean, std ', mean_y, std_y)
 
-# h(w) = X * sita
-sita = np.zeros(shape=[M + 1, 1], dtype=np.float32)
+y = (y - mean_y) / std_y
+
+# h(w) = X * theta
+theta = np.zeros(shape=[M + 1, 1], dtype=np.float32)
 
 count_list = []
 loss_list = []
-sita0_list = []
-sita1_list = []
+theta0_list = []
+theta1_list = []
 
 
-def GD(sita):
-    epoch = 3500000
-    lr = 4e-7
+def GD(theta):
+    epoch = 1000
+    lr = 0.5
     for i in range(epoch + 1):
-        h = np.dot(X, sita)
+        h = np.dot(X, theta)
         loss = 0
         grad = np.zeros(shape=[M + 1, 1], dtype=np.float32)
 
@@ -44,62 +51,56 @@ def GD(sita):
         loss /= N
         grad /= N
 
-        if i % 2000 == 0:
+        if i % 100 == 0:
             count_list.append(i)
             loss_list.append(loss)
-            sita0_list.append(sita[0][0])
-            sita1_list.append(sita[1][0])
-            print('step = %d, loss = %f, sita0 = %f, sita1 = %f' % (i, loss, sita[0][0], sita[1][0]))
+            theta0_list.append(theta[0][0])
+            theta1_list.append(theta[1][0])
+            print('step = %d, loss = %f, theta0 = %f, theta1 = %f' % (i, loss, theta[0][0], theta[1][0]))
 
-        sita[0][0] = sita[0][0] - lr * grad[0][0] * 9e5
-        sita[1][0] = sita[1][0] - lr * grad[1][0]
+        theta = theta - lr * grad
 
-    #sita[0][0] = -1599
-    #sita[1][0] = 0.8
-    return sita
+    #theta[0][0] = -1599
+    #theta[1][0] = 0.8
+    return theta
 
-def formula(sita):
+def formula(theta):
     #  正规方程
     XTX = np.dot(X.T, X)
     ni = np.linalg.inv(XTX)
-    sita = np.dot(np.dot(ni, X.T), y)
-    return sita
+    theta = np.dot(np.dot(ni, X.T), y)
+    return theta
 
 
-def result(sita):
-    plt.figure()
+def result(theta):
+
     plt.scatter(x, y)
     plt.xlabel('Year')
     plt.ylabel('Price')
-    print(sita)
-    plt.plot(x, sita[1][0] * x + sita[0][0])
+    print(theta)
+    plt.plot(x, theta[1][0] * x + theta[0][0])
 
-    plt.figure()
     fg2, axes = plt.subplots(2, 2)
     fg_loss = axes[0][0]
-    fg_sita0 = axes[1][0]
-    fg_sita1 = axes[1][1]
+    fg_theta0 = axes[1][0]
+    fg_theta1 = axes[1][1]
     fg_loss.set_xlabel('step')
     fg_loss.set_ylabel('loss')
-    fg_sita0.set_xlabel('step')
-    fg_sita0.set_ylabel('sita0')
-    fg_sita1.set_xlabel('step')
-    fg_sita1.set_ylabel('sita1')
+    fg_theta0.set_xlabel('step')
+    fg_theta0.set_ylabel('theta0')
+    fg_theta1.set_xlabel('step')
+    fg_theta1.set_ylabel('theta1')
     fg_loss.plot(count_list, loss_list)
-    fg_sita0.plot(count_list, sita0_list)
-    fg_sita1.plot(count_list, sita1_list)
+    fg_theta0.plot(count_list, theta0_list)
+    fg_theta1.plot(count_list, theta1_list)
 
     plt.show()
 
 
 def main():
-    gd = GD(sita); result(gd)
-    #form = formula(sita); result(form)
+    gd = GD(theta); result(gd)
+    #form = formula(theta); result(form)
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
